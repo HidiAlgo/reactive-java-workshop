00fb0a7c68fde2cc5d8ae8e154236014bc07f448
HidiAlgo <hashan.mahesh01@gmail.com>
2024-12-24 19:22:15 +0530

    Exercise five completed

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -9,13 +12,30 @@ public class Exercise5 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+//        ReactiveSources.intNumbersFlux().subscribe(
+//                results -> System.out.println(results),
+//                error -> System.out.println(error.getMessage()),
+//                () -> System.out.println("Done")
+//        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        MySubscriber<Integer> subscriber = new MySubscriber<>();
+        ReactiveSources.intNumbersFlux().subscribe(subscriber);
 
         System.out.println("Press a key to end");
         System.in.read();
     }
 
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+    public void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribe happened");
+        request(1);
+    }
+
+    public void hookOnNext(T t) {
+        System.out.println(t.toString() + " received.");
+        request(1);
+    }
 }
